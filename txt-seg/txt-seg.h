bb236093f12a39ba208f1bd9b1d2b1e64e23f502
t-k- <clock126@126.com>
2015-12-25 02:15:20 -0500

    should use Unicode for DAT

--- txt-seg/txt-seg.h
+++ txt-seg/txt-seg.h
@@ -1,12 +1,13 @@
 #include <stddef.h>
+#include <wchar.h>
 #include "config.h"
 #include "list.h"
 
 struct term_list_node {
-	char             term[MAX_TERM_STR_LEN];
+	wchar_t          term[MAX_TERM_STR_LEN];
 	struct list_node ln;
 };
 
 int   text_segment_init();
-list  text_segment(char *text, size_t arr_len);
+list  text_segment(wchar_t *text, size_t len);
 void  text_segment_free();
