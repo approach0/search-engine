b8800e36cecff8a9007b642a2fa0d9037139f8b7
t-k- <clock126@126.com>
2015-12-26 18:38:43 -0500

    tree-index interface

---
+++ tree-index/tree-index.h
@@ -0,0 +1,35 @@
+#ifndef TREE_INDEX_H
+#define TREE_INDEX_H
+
+#include "../config/config.h"
+
+struct dir_path_item {
+	char             name[MAX_TR_INDEX_DIR_PATH_LEN];
+	struct list_node ln;
+};
+
+struct level_postinglist_item {
+	FILE             *fh; /* posting list FILE handle */
+	struct list_node  ln;
+};
+
+enum level_merge_ret_t {
+	LEVEL_MERGE_CONTINUE,
+	LEVEL_MERGE_STOP
+};
+
+typedef enum level_merge_ret_t 
+(*level_merge)(list level_postinglists, void *args);
+
+void *tree_index_open(const char *index_path);
+
+FILE *
+tree_index_map_postinglist(void *tr_index, char *dir_path);
+
+int
+tree_index_level_merge(void *tr_index, list dir_path_list,
+                       level_merge callbk, void *args);
+
+void  tree_index_close(void *tr_index);
+
+#endif
