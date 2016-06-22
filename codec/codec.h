779bc74ccd991a0eed336da9bf4f65f1ad79062c
yzhan018 <yzhan@udel.edu>
2016-06-21 20:29:39 -0400

    added FORDELTA codec

--- codec/codec.h
+++ codec/codec.h
@@ -9,6 +9,7 @@ struct for_delta_args {
 	int pack_size;
 	/* ...... */
 	/* whatever needs for FOR-delta compression */
+    unsigned b;
 };
 
 struct codec {
@@ -16,6 +17,6 @@ struct codec {
 	void             *args; /* algorithm-dependent arguments */
 };
 
-size_t codec_compress(struct codec*, const uint32_t*, size_t, void*);
+size_t codec_compress(struct codec*, const uint32_t*, size_t, int*);
 
-size_t codec_decompress(struct codec*, void*, uint32_t*, size_t);
+size_t codec_decompress(struct codec*, int*, uint32_t*, size_t);
