188d0cd04800d071d4e9c0212b13bb62eb629bb8
t-k- <clock126@126.com>
2016-08-24 18:45:25 +0800

    Correct free() prototype, no unfree-- on NULL ptr.
    Also add tot_allocs counter.

--- mhook/mhook.c
+++ mhook/mhook.c
@@ -2,43 +2,61 @@
 #include "mhook.h"
 
 static uint64_t unfree = 0;
+static uint64_t tot_allocs = 0;
 
 uint64_t mhook_unfree()
 {
 	return unfree;
 }
 
-void mhook_print_unfree()
+uint64_t mhook_tot_allocs()
 {
-	printf("Unfree mallocs: %lu.\n", unfree);
+	return unfree;
 }
 
+void mhook_print_unfree()
+{
+	printf("Total memory allocs: %lu.\n", tot_allocs);
+	printf("Unfree memory allocs: %lu.\n", unfree);
+}
+
+/* malloc() hook */
 void *__real_malloc(size_t);
 
 void *__wrap_malloc(size_t c)
 {
 	void *p = __real_malloc(c);
 
-	if (p)
+	if (p) {
 		unfree++;
+		tot_allocs++;
+	}
+
 	return p;
 }
 
-void *__real_free(size_t);
-
-void *__wrap_free(size_t c)
-{
-	unfree--;
-	return __real_free(c);
-}
-
+/* calloc() hook */
 void *__real_calloc(size_t, size_t);
 
 void *__wrap_calloc(size_t nmemb, size_t size)
 {
 	void *p = __real_calloc(nmemb, size);
 
-	if (p)
+	if (p) {
 		unfree++;
+		tot_allocs++;
+	}
+
 	return p;
+}
+
+/* free() hook */
+void __real_free(void*);
+
+void __wrap_free(void *p)
+{
+	if (p)
+		unfree--;
+
+	__real_free(p);
 }
--- mhook/mhook.h
+++ mhook/mhook.h
@@ -3,8 +3,10 @@
 #include <malloc.h>
 
 uint64_t mhook_unfree();
+uint64_t mhook_tot_allocs();
+
 void mhook_print_unfree();
 
 void *__wrap_malloc(size_t);
-void *__wrap_free(size_t);
 void *__wrap_calloc(size_t, size_t);
+void __wrap_free(void*);
