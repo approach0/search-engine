5b0d1bee864dd67ddc28c79fab424f69537c4f21
lmffeexd <lmffeexd@sharklasers.com>
2016-12-02 19:40:17 +0000

    Update quiz-list.js
    Another question

--- demo/web/quiz-list.js
+++ demo/web/quiz-list.js
@@ -5,7 +5,7 @@ var quiz_list = [
 			"Okay, find all solutions for ...",
 			"Wait, what is this? It looks terrible!",
 			"Honestly, I hate my math homework.",
-			"Hmmm... Can I <a href=\"#\">search it</a> on Internet?"
+			"Hmmm... Can I <a href=\"#\">search it</a> on the Internet?"
 		],
 		"search":  "$(1+x+\\frac 1 2 x^2)e^{-x}$, solution"
 	},
@@ -15,7 +15,7 @@ var quiz_list = [
 			"No way, I didn't understand today's class at all",
 			"There is no chance I can solve this problem",
 			"I guess I shall give up this for some sleep time...",
-			"Wait... Can I <a href=\"#\">search it</a> on Internet?"
+			"Wait... Can I <a href=\"#\">search it</a> on the Internet?"
 		],
 		"search":  "stochastic, $\\ln(1+ X_t^2)$"
 	},
@@ -25,7 +25,7 @@ var quiz_list = [
 			"Hmmm... This inequality looks simple.",
 			"But it does not mean I know how to prove.",
 			"Stop thinking, you math idiot",
-			"Wait... Can I <a href=\"#\">search it</a> on Internet?"
+			"Wait... Can I <a href=\"#\">search it</a> on the Internet?"
 		],
 		"search":  "$4k < 2^k$, by induction"
 	},
@@ -35,7 +35,7 @@ var quiz_list = [
 			"Have I learned metric space before?",
 			"How come I always have zero impression on math concept...",
 			"This should been taught in today's class, I am 100% confident on that!",
-			"Hmmm... Can I <a href=\"#\">search it</a> on Internet?"
+			"Hmmm... Can I <a href=\"#\">search it</a> on the Internet?"
 		],
 		"search":  "$\\min(d(x,y),2)$, metric space"
 	},
@@ -46,7 +46,7 @@ var quiz_list = [
 			"I can use my computer to enumerate...",
 			"... (10 minutes after) ...",
 			"OK, I give up. Why borther list them <b>all</b>?",
-			"Is that possible to <a href=\"#\">search it</a> on Internet?"
+			"Is that possible to <a href=\"#\">search it</a> on the Internet?"
 		],
 		"search":  "all positive integers, $i^5 + j^6 = k^7$"
 	},
@@ -56,17 +56,27 @@ var quiz_list = [
 			"Let me think...",
 			"It reminds me when I drop my vanilla cone to the floor last time",
 			"Can I just skip to the next question?",
-			"Or if it is possible to <a href=\"#\">search the answer</a> on Internet?"
+			"Or if it is possible to <a href=\"#\">search the answer</a> on the Internet?"
 		],
 		"search":  "cone, $z=\\sqrt{x^2+y^2}$, plane, $z=1+y$"
 	},
 	{ /* 6 */
+		"Q": "What is the value of the following limit [imath]\displaystyle\lim_{n\to\infty} e^{-n} \sum_{k=0}^{n} \frac{n^k}{k!}[/imath] ?",
+		"hint": [
+		        "Where do I start?",
+			"This seems impossible...",
+			"I'm running out of time, guess I shall give up some sleep time again...",
+	       		"Or is it possible to <a href=\"#\">find the answer</a> on the Internet?"	
+		],
+		"search": "$\lim_{n\to\infty} e^{-n} \sum_{k=0}^{n} \frac{n^k}{k!}$"
+        },	           
+	{ /* 7 */
 		"Q": "Can you prove [imath](|x+y|)^2<(|x| + |y|)^2[/imath] ?",
 		"hints": [
 			"... This question looks familiar to me",
 			"It must be somewhere in the textbook...",
 			"Hold on, where is my textbook? Did I left it in my classroom ?!",
-			"OKay... Let's see if I can <a href=\"#\">search this question</a> on Internet?"
+			"OKay... Let's see if I can <a href=\"#\">search this question</a> on the Internet?"
 		],
 		"search":  "Prove, $\\left(|x+y|\\right)^2<\\ ?$"
 	}
