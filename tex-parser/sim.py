5fdfa56f546ad2898ee3f898c02c9503081043ee
t-k- <clock126@126.com>
2018-01-27 15:52:10 -0500

    correct scoring.

--- tex-parser/sim.py
+++ tex-parser/sim.py
@@ -28,12 +28,16 @@ def align(t_q, t_d, qd_map, path_map):
         if t_q[1][i] != t_d[1][i]:
             break
         n_q, n_d = t_q[0][i], t_d[0][i]
-        if (n_q in qd_map and qd_map[n_q] != n_d) or (
-            n_d in dq_map and dq_map[n_d] != n_q):
-            return 0
-        qd_map[n_q] = n_d
-        dq_map[n_d] = n_q
-        cnt += 1
+        if n_q in qd_map:
+            if qd_map[n_q] != n_d:
+                return 0
+        elif n_d in dq_map:
+            if dq_map[n_d] != n_q:
+                return 0
+        else:
+            qd_map[n_q] = n_d
+            dq_map[n_d] = n_q
+            cnt += 1
     path_map[t_q[0][0]] = t_d[0][0]
     return cnt
 
@@ -48,35 +52,42 @@ def get_hitlist(query, doc):
     return hitlist
 
 def similarity(query, doc, hitlist):
-	queue = [(-1, -1, {}, {}, 0)]
-	level_hit = False
-	last_i = 0
-	best_map = {}
-	best_score = 0
-	while len(queue):
-		i, j, node_map, path_map, score = queue.pop(0)
-		if i != last_i:
-			level_max = 0
-			level_hit = False
-		last_i = i
-		if i != -1:
-			q = query[i]
-			d = doc[j]
-			prefix = align(q, d, node_map, path_map)
-			# print(i, j, path_map)
-			if prefix > 2:
-				level_hit = True
-				score += prefix
-				if score > level_max:
-					best_map = path_map
-					best_score = score
-			elif level_hit:
-				continue
-		if i + 1 < len(query):
-			for t in hitlist[i + 1]:
-				n = (i + 1, t, dict(node_map), dict(path_map), score)
-				queue.append(n);
-	return (best_score, best_map)
+    queue = [(-1, -1, {}, {}, 0)]
+    last_i = 0
+    level_hit = False
+    level_max = 0
+    best_map = {}
+    best_score = 0
+    while len(queue):
+        i, j, node_map, path_map, score = queue.pop(0)
+        if i != last_i:
+            level_max = 0
+            level_hit = False
+        last_i = i
+        if i != -1:
+            q = query[i]
+            d = doc[j]
+            copy_node_map = dict(node_map)
+            cnt = align(q, d, node_map, path_map)
+            if cnt > 2:
+                level_hit = True
+                score += cnt
+                # print(score, len(node_map))
+                if score > level_max:
+                    level_max = score
+                    best_map = path_map
+                    best_score = score
+            else:
+                if level_hit:
+                    continue
+                else:
+                    node_map = copy_node_map;
+
+        if i + 1 < len(query):
+            for t in hitlist[i + 1]:
+                n = (i + 1, t, dict(node_map), dict(path_map), score)
+                queue.append(n);
+    return (best_score, best_map)
 
 query = parse_file(arg1)
 doc   = parse_file(arg2)
@@ -87,4 +98,4 @@ print(query, end='\n')
 print(doc, end='\n')
 print(*hitlist, sep='\n', end="\n")
 print(align)
-print('Score: ' + str(score))
+print('Score: %d, %d/%d' % (score, len(align), len(hitlist)))
