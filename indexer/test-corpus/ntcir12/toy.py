9ff6bca0e3a970e3230a33905c448178b478d416
t-k- <clock126@126.com>
2018-03-05 13:10:49 -0500

    modify toy.py, no need to collapse exp in same doc.

--- indexer/test-corpus/ntcir12/toy.py
+++ indexer/test-corpus/ntcir12/toy.py
@@ -6,56 +6,35 @@ import time
 fname = "NTCIR12_latex_expressions.txt"
 #fname = "toy.txt"
 
-limit = -1
-first_time = True
-
 def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+	with open(fname) as f:
+		for i, l in enumerate(f):
+			pass
+	return i + 1
 total_lines = file_len(fname)
 
-g_cnt = 0
-
 def send_json(json_obj):
-    global g_cnt
-    headers = {'content-type': 'application/json'}
-    r = requests.post('http://localhost:8934/index',
-        json=json_obj,
-        headers=headers)
-    print(r.status_code)
-    # time.sleep(.10)
-    g_cnt += 1
-    print("%d / %d" % (g_cnt, total_lines))
-    if limit != -1 and g_cnt > limit:
-        quit()
+	headers = {'content-type': 'application/json'}
+	r = requests.post('http://localhost:8934/index',
+		json=json_obj,
+		headers=headers)
+	print(r.status_code)
 
 with open(fname) as f:
-    last_docid = 'undefined'
-    json_obj = {"text": '', "url": 'undefined'}
+	cnt = 0
+	for line in f:
+		line = line.strip('\n')
+		fields = line.split()
+		docid_and_pos = fields[0]
+		latex = ' '.join(fields[1:])
+		latex = latex.replace('% ', '')
+		docid = docid_and_pos
+		# print(latex)
 
-    for line in f:
-        line = line.strip('\n')
-        fields = line.split()
-        docid_and_pos = fields[0]
-        latex = ' '.join(fields[1:])
+		json_obj = {}
+		json_obj['url'] = docid;
+		json_obj['text'] = '[imath]' + latex + '[/imath]  ';
+		send_json(json_obj)
 
-        docid = docid_and_pos
-        # docid = docid_and_pos.split(':')[0]
-        # pos = docid_and_pos.split(':')[1]
-
-        if first_time or docid == last_docid:
-            first_time = False
-        else:
-            print(json_obj)
-            send_json(json_obj)
-            json_obj = {"text": '', "url": 'undefined'}
-
-        json_obj['url'] = docid;
-        json_obj['text'] += '[imath]' + latex + '[/imath]  ';
-        last_docid = docid
-
-print(json_obj)
-send_json(json_obj)
-json_obj = {"text": '', "url": 'undefined'}
+		cnt += 1
+		print("%d / %d" % (cnt, total_lines))
