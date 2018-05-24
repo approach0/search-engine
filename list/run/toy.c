807d8140aff05969237a89bd9910df2b001972b3
t-k- <clock126@126.com>
2018-05-24 15:56:19 -0400

    toy

--- list/run/toy.c
+++ list/run/toy.c
@@ -36,7 +36,17 @@ int list_next(list li, struct list_iterator *g)
 	}
 }
 
-#define tostring(_e) (# _e)
+
+#define FOREACH(_iter, _type, _collection, _stmts) \
+	{ \
+		struct _type ## _iterator _iter = _type ## _generator(_collection); \
+		do { \
+			_stmts \
+		} while (_type ## _next(_collection, &_iter)); \
+	}
+
+#define LIST_ITEM(_item, _type, _iter) \
+	_type *_item = MEMBER_2_STRUCT(_iter.cur.now, _type, ln)
 
 int main()
 {
@@ -47,53 +57,43 @@ int main()
 	/* initialize list */
 	for (i = 0; i < 9; i++) {
 		p = malloc(sizeof(struct T));
-		p->i = i;
+		p->i = (i * 2) %3;
 		LIST_NODE_CONS(p->ln);
 		list_insert_one_at_tail(&p->ln, &li, NULL, NULL);
 	}
 
-	/* generic for */
-	{
-		struct list_iterator g = list_generator(li);
-		do {
-			struct T *t = MEMBER_2_STRUCT(g.cur.now, struct T, ln);
-			printf("%d\n", t->i);
-		} while (list_next(li, &g));
-	}
+//	/* generic for */
+//	{
+//		struct list_iterator g = list_generator(li);
+//		do {
+//			struct T *t = MEMBER_2_STRUCT(g.cur.now, struct T, ln);
+//			printf("- %d\n", t->i);
+//	
+//			/* generic for */
+//			{
+//				struct list_iterator g = list_generator(li);
+//				do {
+//					struct T *t = MEMBER_2_STRUCT(g.cur.now, struct T, ln);
+//					printf("-- %d\n", t->i);
+//				} while (list_next(li, &g));
+//			}
+//		} while (list_next(li, &g));
+//	}
 
-	/* generic for */
-	{
-		struct list_iterator g = list_generator(li);
-		do {
-			__typeof__(p) g;
-			struct T *t = MEMBER_2_STRUCT(g.cur.now, struct T, ln);
-			printf("%d\n", t->i);
-		} while (list_next(li, &g));
-	}
+	int idx = 0;
+	FOREACH(iter1, list, li,
+		FOREACH(iter2, list, li,
+			LIST_ITEM(t1, struct T, iter1);
+			LIST_ITEM(t2, struct T, iter2);
+			printf("[%d] %d -> %d\n", idx, t1->i, t2->i);
 
-	/* generic for */
-	{
-		struct list_iterator g = list_generator(li);
-		do {
-			struct T *t = MEMBER_2_STRUCT(g.cur.now, struct T, ln);
-			printf("- %d\n", t->i);
+		)
 
-			/* generic for */
-			{
-				struct list_iterator g = list_generator(li);
-				do {
-					struct T *t = MEMBER_2_STRUCT(g.cur.now, struct T, ln);
-					printf("-- %d\n", t->i);
-				} while (list_next(li, &g));
-			}
-		} while (list_next(li, &g));
-	}
-
-	// for_each(node, list, li) {
-	// 	// node = g.cur.now;
-	// 	struct T *t = MEMBER_2_STRUCT(node, struct T, ln);
-	// 	printf("%d\n", t->i);
-	// }
+		if (idx == 4) {
+			break;
+		}
+		idx += 1;
+	)
 	
 	/* release memory */
 	list_release(&li);
