b3d9d04615b9d828c7cd2c85d957032f59fffa22
t-k- <clock126@126.com>
2018-07-06 11:21:32 -0400

    seperate two relevance level evaluation scripts

--- eval-trec-results-fullrel.sh
+++ eval-trec-results-fullrel.sh
@@ -4,7 +4,3 @@ echo 'Full relevance:'
 $cmd -l3 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -v "docno" | grep -E "(^P|bpref)"
 echo 'query specific:'
 $cmd -q -l3 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -E "(bpref|docno)"
-# echo 'Partial relevance:'
-# $cmd -l1 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -v "docno" | grep -E "(^P|bpref)"
-# echo 'query specific:'
-# $cmd -q -l1 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -E "(bpref|docno)"
---
+++ eval-trec-results-partrel.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cmd=./trec_eval
+echo 'Partial relevance:'
+$cmd -l1 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -v "docno" | grep -E "(^P|bpref)"
+echo 'query specific:'
+$cmd -q -l1 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -E "(bpref|docno)"
