5b4c86f1e926aabc53db3f57eb0f589f58e63ab6
t-k- <clock126@126.com>
2018-09-07 17:44:56 -0400

    add output_path option to indexd

--- indexer/run/indexd.c
+++ indexer/run/indexd.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <evhttp.h>
 #include <signal.h>
+#include <getopt.h>
 
+#include "mhook/mhook.h"
 #include "httpd/httpd.h"
 #include "config.h"
 #include "index.h"
@@ -31,22 +33,52 @@ int parser_tex_exception_handler(char *tex, char *msg, uint64_t n_err, uint64_t 
 	return 0;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+	int opt;
 	struct indices indices;
 	doc_id_t max_doc_id;
 	text_lexer lex = lex_eng_file;
-	const char output_path[] = "./tmp";
+	char *output_path = NULL;
 	const char err_output_file[] = "./indexd-parser-error.tmp";
 	unsigned short port = 8934;
 	struct uri_handler uri_handlers[] = {
 		{INDEXD_DEFAULT_URI, httpd_on_recv}
 	};
 	unsigned int len = sizeof(uri_handlers) / sizeof(struct uri_handler);
+
+	while ((opt = getopt(argc, argv, "ho:p:d:")) != -1) {
+		switch (opt) {
+		case 'h':
+			printf("DESCRIPTION:\n");
+			printf("index daemon. \n");
+			printf("\n");
+			printf("USAGE:\n");
+			printf("%s -h | "
+			       "-o <output path> (default is ./tmp)"
+			       "\n", argv[0]);
+			printf("\n");
+			printf("EXAMPLE:\n");
+			printf("%s -o ./some/where\n", argv[0]);
+			goto exit;
+
+		case 'o':
+			output_path = strdup(optarg);
+			break;
+
+		default:
+			printf("bad argument(s). \n");
+			goto exit;
+		}
+	}
+
+	if (output_path == NULL)
+		output_path = strdup("./tmp");
 	
+	printf("opening output dir: %s\n", output_path);
 	if (indices_open(&indices, output_path, INDICES_OPEN_RW)) {
 		fprintf(stderr, "indices open failed.\n");
-		return 1;
+		goto exit;
 	}
 	
 	fh_err_output = fopen(err_output_file, "a");
@@ -68,5 +100,11 @@ close:
 	indices_close(&indices);
 
 	fclose(fh_err_output);
+	
+exit:
+	if (output_path)
+		free(output_path);
+
+	mhook_print_unfree();
 	return 0;
 }
