7d676d894bba47cc1528f0b9b71b785bb904ef4c
t-k- <clock126@126.com>
2018-09-27 11:56:44 -0400

    added eval-trec-results-summary.sh

---
+++ eval-trec-results-summary.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cmd=./trec_eval
+echo 'Full relevance:'
+$cmd -l3 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -v "docno" | grep -E "(^P|bpref)" | head -6
+full=`$cmd -l3 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -v "docno" | grep 'bpref' | awk '{print $3}'`
+echo 'Partial relevance:'
+$cmd -l1 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -v "docno" | grep -E "(^P|bpref)" | head -6
+part=`$cmd -l1 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -v "docno" | grep 'bpref' | awk '{print $3}'`
+
+# copy to clipboard and make it easy to paste on spreadsheet.
+echo "${part} \t ${full}" | xclip -selection c
