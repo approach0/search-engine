e41aa381adafcd82c66ed1d71df1e1488de72c38
t-k- <clock126@126.com>
2019-01-06 00:10:45 +0800

    In eval scripts, force output query number in numerical order.

--- eval-trec-results-fullrel.sh
+++ eval-trec-results-fullrel.sh
@@ -3,4 +3,4 @@ cmd=./trec_eval
 echo 'Full relevance:'
 $cmd -l3 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -v "docno" | grep -E "(^P|bpref)"
 echo 'query specific:'
-$cmd -q -l3 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -E "(bpref|docno)"
+$cmd -q -l3 NTCIR12_MathWiki-qrels_judge.dat $1 | sort -n | grep -E "(bpref|docno)" | sort -V
--- eval-trec-results-partrel.sh
+++ eval-trec-results-partrel.sh
@@ -3,4 +3,4 @@ cmd=./trec_eval
 echo 'Partial relevance:'
 $cmd -l1 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -v "docno" | grep -E "(^P|bpref)"
 echo 'query specific:'
-$cmd -q -l1 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -E "(bpref|docno)"
+$cmd -q -l1 NTCIR12_MathWiki-qrels_judge.dat $1 | grep -E "(bpref|docno)" | sort -V
