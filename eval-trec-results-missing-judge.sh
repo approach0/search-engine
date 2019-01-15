e1866df04163b7945b0c38960a18753e1e69fa65
t-k- <clock126@126.com>
2019-01-15 16:40:08 +0800

    added eval-trec-results-missing-judge.sh

---
+++ eval-trec-results-missing-judge.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+cmd=trec_eval
+url=http://localhost:3838/get
+low_uri=lowerbound/judge.dat
+upp_uri=upperbound/judge.dat
+
+file_path=$1
+run_name=$2
+
+rm -f ./judge.dat
+wget $url/$run_name/$upp_uri
+
+echo 'Partial relevance (lower bound): '
+$cmd -l1 NTCIR12_MathWiki-qrels_judge.dat $file_path | grep -E "^P" | head -4
+
+echo 'Partial relevance (Condensed): '
+$cmd -J -l1 NTCIR12_MathWiki-qrels_judge.dat $file_path | grep -E "^P" | head -4
+
+echo 'Partial relevance (upper bound): '
+$cmd -l1 ./judge.dat $file_path | grep -E "^P" | head -4
+
+echo 'Full relevance (lower bound): '
+$cmd -l3 NTCIR12_MathWiki-qrels_judge.dat $file_path | grep -E "^P" | head -4
+
+echo 'Full relevance (Condensed): '
+$cmd -J -l3 NTCIR12_MathWiki-qrels_judge.dat $file_path | grep -E "^P" | head -4
+
+echo 'Full relevance (upper bound): '
+$cmd -l3 ./judge.dat $file_path | grep -E "^P" | head -4
