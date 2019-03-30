9c596ab0aafb5513c3711edc8b3b9aa67ab2c27a
t-k- <clock126@126.com>
2019-03-30 13:26:23 +0800

    remove dead search/config.h macros.

--- search/config.h
+++ search/config.h
@@ -1,20 +1,15 @@
 /* for MAX_TXT_SEG_BYTES & MAX_TXT_SEG_LEN */
 #include "txt-seg/config.h"
-#define MAX_TERM_BYTES MAX_TXT_SEG_BYTES
 
 /* consider both math & term */
 #define MAX_QUERY_BYTES     (MAX_TXT_SEG_BYTES * 32)
 #define MAX_QUERY_WSTR_LEN  (MAX_TXT_SEG_LEN * 32)
-
-#define MAX_POSTINGS_PER_MATH (MAX_MERGE_POSTINGS >> 3)
 
 #define SNIPPET_PADDING    320
 #define MAX_TEX_STRLEN    (MAX_TXT_SEG_BYTES * 32)
 #define MAX_SNIPPET_SZ     8192
 
 //#define DEBUG_SNIPPET
-
-//#define DEBUG_POST_MERGE
 
 /* max mark score, type of mnc_score_t */
 #define MNC_MARK_BASE_SCORE 90
@@ -40,12 +35,6 @@
 
 // #define PRINT_RECUR_MERGING_ITEMS
 
-// #define DEBUG_MATH_EXPR_SEARCH
-
-//#define DEBUG_STATS_HOT_HIT
-
-
-// #define QUIET_SEARCH
 // #define MERGE_TIME_LOG "merge.runtime.dat"
 
 // #define DEBUG_MERGE_LIMIT_ITERS 900
@@ -70,28 +59,12 @@
 //#define DEBUG_PROXIMITY
 #define ENABLE_PROXIMITY_SCORE
 
-#define MATH_PREFIX_SEARCH_ONLY /* determine lr_search or prefix_search */
-
-//#define ENABLE_PARTIAL_MATCH_PENALTY
-
 #define MAX_MATH_OCCURS 16
 
 #define MAX_TOTAL_OCCURS 32
 
 //#define DEBUG_HILIGHT_SEG_OFFSET
 //#define DEBUG_HILIGHT_SEG
-
-//#define DEBUG_MATH_SCORE_POSTING
-#define VERBOSE_SEARCH
-
-//#define DEBUG_MATH_SEARCH
-
-//#define DEBUG_PRINT_TARGET_DOC_MATH_SCORE
-
-#define MAX_MATH_EXP_SEARCH_ITEMS 9301000 /* dead */
-#define MAX_MATH_EXP_SEARCH_DIRS  521     /* dead */
-
-#define MATCH_DIM_WEIGHT 10000
 
 /* prefix search */
 #define MAX_NODE_IDS 4096
@@ -113,8 +86,6 @@
 #define HIGHLIGHT_MTREE_ALLOC (MAX_MTREE + 3)
 
 #define MAX_LEAVES  64
-
-//#define PRINT_MATH_POST_TYPE
 
 // #define SKIP_SEARCH /* for debug */
 // #define PRINT_RECUR_MERGING_ITEMS /* for debug */
