7c9c4fa3bc704d2263c42bf76d262e9b38b3c8eb
t-k- <clock126@126.com>
2019-04-02 14:16:47 +0800

    added --begin-from option in json-feeder.py

--- indexer/scripts/json-feeder.py
+++ indexer/scripts/json-feeder.py
@@ -30,14 +30,16 @@ def get_n_files(corpus):
 
 # main #
 parser = argparse.ArgumentParser(description='Approach0 indexd json feeder.')
+parser.add_argument('--begin-from', help='begin from specified count of files.', type=int)
 parser.add_argument('--maxfiles', help='limit the max number of files to be indexed.', type=int)
-parser.add_argument('--corpus-path', help='corpus path.', type=str)
-parser.add_argument('--indexd-url', help='indexd URL (optional).', type=str)
+parser.add_argument('--corpus-path', help='corpus path. (required)', type=str)
+parser.add_argument('--indexd-url', help='indexd URL.', type=str)
 args = parser.parse_args()
 
 url= args.indexd_url if args.indexd_url else 'http://localhost:8934/index'
 corpus = args.corpus_path if args.corpus_path else ''
 maxfiles = args.maxfiles if args.maxfiles else -1
+begin = args.begin_from if args.begin_from else 0
 
 print('Indexd URL: ' + url)
 
@@ -47,6 +49,9 @@ print('%u files in total.' % N)
 
 cnt = 0
 for dirname, basename in each_json_file(corpus, maxfiles):
+	cnt += 1
+	if cnt < begin:
+		continue
 	path = dirname + '/' + basename
 	with open(path, 'r') as fh:
 		try:
@@ -55,5 +60,4 @@ for dirname, basename in each_json_file(corpus, maxfiles):
 		except Exception as err:
 			print(err)
 			break
-		print(f'[{cnt + 1:,d} / {N:,d}] doc#{docid}: {j["url"]}')
-	cnt += 1
+		print(f'[{cnt:,d} / {N:,d}] doc#{docid}: {j["url"]}')
