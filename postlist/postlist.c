614516c9fe6b7664a8c0be39cc85986242687a14
t-k- <clock126@126.com>
2019-04-28 21:45:53 +0800

    remove non-reentrant in-memory iterator functions.

--- postlist/postlist.c
+++ postlist/postlist.c
@@ -167,161 +167,6 @@ static void forward_cur(struct postlist_node **cur)
 	*cur = MEMBER_2_STRUCT(next, struct postlist_node, sn);
 }
 
-static void rebuf_cur(struct postlist *po)
-{
-	struct postlist_node *cur = po->cur;
-
-	if (cur) {
-		/* refill buffer */
-		memcpy(po->buf, cur->blk, cur->blk_sz);
-		po->buf_end = cur->blk_sz;
-
-		/* invoke rebuf callback */
-		po->on_rebuf(po->buf, &po->buf_end, po->buf_arg);
-	} else {
-		/* reset buffer variables anyway */
-		po->buf_end = 0;
-	}
-
-	po->buf_idx = 0;
-}
-
-bool postlist_start(void *po_)
-{
-	struct postlist *po = (struct postlist*)po_;
-
-	if (po->head == NULL)
-		return 0;
-
-	/* setup buffer for rebuffering */
-	SETUP_BUFFER(po);
-
-	/* initial buffer filling */
-	po->cur = po->head;
-	rebuf_cur(po);
-
-	return (po->buf_end != 0);
-}
-
-bool postlist_next(void *po_)
-{
-	struct postlist *po = (struct postlist*)po_;
-
-	if (po->buf_idx + po->item_sz < po->buf_end) {
-		po->buf_idx += po->item_sz;
-		return 1;
-	} else if (po->buf_end != 0) {
-		/* next block */
-		forward_cur(&po->cur);
-		/* reset buffer */
-		rebuf_cur(po);
-		return (po->buf_end != 0);
-	}
-
-	return 0;
-}
-
-void* postlist_cur_item(void *po_)
-{
-	struct postlist *po = (struct postlist*)po_;
-	return po->buf + po->buf_idx;
-}
-
-int postlist_terminates(void *po_)
-{
-	struct postlist *po = (struct postlist*)po_;
-	return (po->buf_end == 0);
-}
-
-//#define DEBUG_POSTLIST
-bool postlist_jump(void *po_, uint64_t target)
-{
-	/* Notice: If target is going back, jump() will go to the end. */
-	struct postlist *po = (struct postlist*)po_;
-	struct skippy_node *jump_to;
-	uint64_t           *curID, *endID;
-
-#ifdef DEBUG_POSTLIST
-	int debug_cnt = 0;
-#endif
-	endID = (uint64_t*)(po->buf + po->buf_end - po->item_sz);
-
-#ifdef DEBUG_POSTLIST
-	assert(po->buf_end != 0);
-	printf("jump to target=%lu, endID=%lu.\n", target, *endID);
-#endif
-	if (target <= *endID)
-		goto glide;
-
-	/* using skip-list */
-	//jump_to = skippy_node_jump(&po->cur->sn, target);
-	jump_to = skippy_node_lazy_jump(&po->cur->sn, target);
-
-#ifdef DEBUG_POSTLIST
-	assert(jump_to != &po->cur->sn); /* must be a different node */
-#endif
-
-	/* this is a new block, update pointer and rebuf. */
-	po->cur = MEMBER_2_STRUCT(jump_to, struct postlist_node, sn);
-	rebuf_cur(po);
-
-	/* at this point, there shouldn't be any problem to access
-	 * current posting item:
-	 * case 1: if had jumped to a new block, the buffer must
-	 * be non-empty.
-	 * case 2: if we stay in the old block, it is guaranteed
-	 * that current posting item is a valid pointer. */
-
-	/* seek in the current block until we get to an ID greater or
-	 * equal to target ID. */
-glide:
-	do {
-		/* docID must be the first member of structure */
-		curID = (uint64_t*)postlist_cur_item(po);
-#ifdef DEBUG_POSTLIST
-		printf("glide to: %lu (%u)\n", *curID, (*curID) >> 32);
-#endif
-		if (*curID >= target) {
-#ifdef DEBUG_POSTLIST
-			printf("glides: %d\n", debug_cnt);
-#endif
-			return 1;
-		}
-
-#ifdef DEBUG_POSTLIST
-		debug_cnt ++;
-#endif
-	} while (postlist_next(po));
-
-	return 0;
-}
-//#undef DEBUG_POSTLIST
-
-void postlist_finish(void *po_)
-{
-	struct postlist *po = (struct postlist*)po_;
-	FREE_BUFFER(po);
-}
-
-#include "postlist-codec/postlist-codec.h"
-
-void print_postlist(struct postlist *po)
-{
-	struct math_postlist_item *pi;
-	postlist_start(po);
-	PTR_CAST(codec, struct postlist_codec, po->buf_arg);
-
-	do {
-		pi = postlist_cur_item(po);
-		postlist_print(pi, 1, codec->fields);
-
-	} while (postlist_next(po));
-
-	postlist_finish(po);
-}
-
-//////////////////////////////////////
-
 static void
 postlist_iter_rebuf(struct postlist_iterator *iter)
 {
@@ -383,6 +228,14 @@ void* postlist_iter_cur_item(struct postlist_iterator* iter)
 	return iter->buf + iter->buf_idx;
 }
 
+uint64_t postlist_iter_cur(struct postlist_iterator* iter)
+{
+	if (iter->buf_end == 0)
+		return UINT64_MAX;
+	else
+		return *(uint64_t*)postlist_iter_cur_item(iter);
+}
+
 int
 postlist_iter_jump32(struct postlist_iterator* iter, uint32_t target)
 {
@@ -439,4 +292,19 @@ void postlist_iter_free(struct postlist_iterator* iter)
 {
 	free(iter->buf);
 	free(iter);
+}
+
+#include "postlist-codec/postlist-codec.h"
+
+void print_postlist(struct postlist *po)
+{
+	struct math_postlist_item *pi;
+	PTR_CAST(codec, struct postlist_codec, po->buf_arg);
+
+	postlist_iter_t iter = postlist_iterator(po);
+	do {
+		pi = (struct term_posting_item*)postlist_iter_cur_item(iter);
+		postlist_print(pi, 1, codec->fields);
+	} while (postlist_iter_next(iter));
+	postlist_iter_free(iter);
 }
--- postlist/postlist.h
+++ postlist/postlist.h
@@ -65,22 +65,14 @@ void postlist_print_info(struct postlist*);
 size_t postlist_write(struct postlist*, const void*, size_t);
 size_t postlist_write_complete(struct postlist*);
 
-/* iterator functions */
-bool  postlist_start(void*);
-bool  postlist_next(void*);
-void* postlist_cur_item(void*);
-uint64_t postlist_cur_item_id(void*);
-bool  postlist_jump(void*, uint64_t);
-void  postlist_finish(void*);
-int postlist_terminates(void*);
-
 void print_postlist(struct postlist *);
 
 /* postlist_iterator functions */
 postlist_iter_t postlist_iterator(struct postlist*);
-int   postlist_empty(struct postlist*);
-int   postlist_iter_next(struct postlist_iterator*);
-void* postlist_iter_cur_item(struct postlist_iterator*);
-int   postlist_iter_jump(struct postlist_iterator*, uint64_t);
-int   postlist_iter_jump32(struct postlist_iterator*, uint32_t);
-void  postlist_iter_free(struct postlist_iterator*);
+int             postlist_empty(struct postlist*);
+int             postlist_iter_next(struct postlist_iterator*);
+uint64_t        postlist_iter_cur(struct postlist_iterator*);
+void*           postlist_iter_cur_item(struct postlist_iterator*);
+int             postlist_iter_jump(struct postlist_iterator*, uint64_t);
+int             postlist_iter_jump32(struct postlist_iterator*, uint32_t);
+void            postlist_iter_free(struct postlist_iterator*);
