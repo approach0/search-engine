a88e4bd3d88974633f438d61c69f3f5152abb81f
t-k- <clock126@126.com>
2019-05-10 12:49:43 +0800

    diff config.h

--- search/config.h
+++ search/config.h
@@ -31,12 +31,9 @@
 // #define DEBUG_HIT_SCORE_INSPECT /* particular docID scoring inspect */
 // #define DEBUG_PRINT_QRY_STRUCT
 
-// #define MATH_SKIP_SET_FAST_SELECTION
 // #define DEBUG_MATH_SKIP_SET_SELECTION
 
 // #define PRINT_RECUR_MERGING_ITEMS
-
-// #define MERGE_TIME_LOG "merge.runtime.dat"
 
 // #define DEBUG_MATH_PRUNING
 
@@ -48,20 +45,13 @@
 
 #define MATH_OCCUR_ONLY_ONE
 
-/* wildcards paths expand as single symbol */
-//#define WILDCARD_PATH_QUERY_EXPAND_ENABLE
-
-//#define MATH_PRUNING_MIN_THRESHOLD_FACTOR 0.30f /* aggressive */
-#define MATH_PRUNING_MIN_THRESHOLD_FACTOR .15f  /* conservative */
-//#define MATH_PRUNING_MIN_THRESHOLD_FACTOR .00f  /* rank-safe */
+#define MATH_PRUNING_MIN_THRESHOLD_FACTOR 0.f
 
 #ifdef DEBUG_MATH_PRUNING
 	#define DEBUG_MERGE_LIMIT_ITERS UINT64_MAX
 	#define RANK_SET_DEFAULT_VOL 200
 #else
-	#define RANK_SET_DEFAULT_VOL 100
-	// #define RANK_SET_DEFAULT_VOL 200
-	// #define RANK_SET_DEFAULT_VOL 1000
+	#define RANK_SET_DEFAULT_VOL ?
 #endif
 
 #define DEFAULT_RES_PER_PAGE 10
