6174ac34beccadaf913cd02330e7222ffec33137
t-k- <clock126@126.com>
2019-05-10 14:59:16 +0800

    report wild vs. non-wild seperately.

--- stats-efficiency-summary.py
+++ stats-efficiency-summary.py
@@ -13,18 +13,27 @@ with open(runtime_dat, 'r') as runtime_fh:
         runtime.append(float(fields[1]))
     output = {}
 
-    runtime20 = runtime[0:20]
-    output['mean20'] = statistics.mean(runtime20)
-    output['median20'] = statistics.median(runtime20)
-    output['max20'] = max(runtime20)
-    output['min20'] = min(runtime20)
-    output['std20'] = statistics.stdev(runtime20)
+    runtime02 = runtime[0:20]
+    output['avg-02'] = statistics.mean(runtime02)
+    output['med-02'] = statistics.median(runtime02)
+    output['max-02'] = max(runtime02)
+    output['min-02'] = min(runtime02)
+    output['std-02'] = statistics.stdev(runtime02)
 
-    output['mean'] = statistics.mean(runtime)
-    output['median'] = statistics.median(runtime)
-    output['max'] = max(runtime)
-    output['min'] = min(runtime)
-    output['std'] = statistics.stdev(runtime)
+    runtime24 = runtime[20:40]
+    output['avg-24'] = statistics.mean(runtime24)
+    output['med-24'] = statistics.median(runtime24)
+    output['max-24'] = max(runtime24)
+    output['min-24'] = min(runtime24)
+    output['std-24'] = statistics.stdev(runtime24)
+
+    runtime04 = runtime[:]
+    output['avg-04'] = statistics.mean(runtime04)
+    output['med-04'] = statistics.median(runtime04)
+    output['max-04'] = max(runtime04)
+    output['min-04'] = min(runtime04)
+    output['std-04'] = statistics.stdev(runtime04)
+
     output['runtime'] = runtime
     output_json = json.dumps(output, sort_keys=True, indent=4)
     print(output_json)
