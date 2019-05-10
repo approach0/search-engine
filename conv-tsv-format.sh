c1fadaef10a956c5de57ea7c58fe1c3d8dbc9cbb
t-k- <clock126@126.com>
2019-05-10 23:19:46 +0800

    added conv-tsv-format.sh

---
+++ conv-tsv-format.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+tsv_file="$1"
+while read l; do
+	awk '{print $6, "&", $4 "-" $2; for (i = 9; i <= 18; i++) printf("& %s", $i); printf("\\\\ \n") }' | sed -e 's/_/-/' -e 's/maxref/MaxRef/' -e 's/gbp/GBP/' -e 's/len/LEN/' -e 's/num/NUM/' -e 's/wiki/Wiki/' -e 's/full-mse/MSE/'
+done < $tsv_file
+echo ""
