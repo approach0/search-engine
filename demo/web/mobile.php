4e247cbe48b530aaa26d064c96414e8573770e38
t-k- <clock126@126.com>
2019-06-07 22:31:44 +0800

    revise mobile.php for SEO

--- demo/web/mobile.php
+++ demo/web/mobile.php
@@ -13,9 +13,19 @@ p {text-align: center;}
 </style>
 </head>
 <body>
-<img style="margin: 0 auto; display: block;" src="images/logo64.png"/>
-<p>Please visit</p>
-<p> <?php echo $_SERVER['SERVER_NAME']; ?> <p>
-<p>on PC or tablet.</p>
+<div>
+	<img style="margin: 0 auto; display: block;" src="images/logo64.png"/>
+	<h3>
+		What is Approach0?
+	</h3>
+	<p>
+	Approach0 is a math-aware search engine.
+	It can query keywords in mathematics formulas or equations.
+	</p>
+</div>
+<div style="padding-top: 100px; font-weight: bold">
+<p>Sorry, math input on mobile platform is not</p>
+<p> supported yet, please visit this site on PC or tablet.</p>
+</div>
 </body>
 </html>
