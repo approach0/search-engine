77a92f5f66ebc783f5c69821418f3ea75436010b
t-k- <clock126@126.com>
2019-07-25 22:23:32 +0800

    modulize codec-buf

---
+++ postlist/codec-buf.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "codec/codec.h"
+#include "codec-buf.h"
+
+void** codec_buf_alloc(uint n, struct codec_buf_struct_info *info)
+{
+	void **buf = malloc(info->n_fields * sizeof(int *));
+
+	for (int j = 0; j < info->n_fields; j++) {
+		buf[j] = malloc(n * info->field_info[j].sz);
+	}
+
+	return buf;
+}
+
+void codec_buf_free(void **buf, struct codec_buf_struct_info *info)
+{
+	for (int j = 0; j < info->n_fields; j++) {
+		free(buf[j]);
+	}
+
+	free(buf);
+}
+
+struct codec_buf_struct_info *codec_buf_struct_info_alloc(int n_fields)
+{
+	struct codec_buf_struct_info *p =
+		malloc(sizeof *p + n_fields * sizeof(p->field_info[0]));
+	p->n_fields = n_fields;
+	return p;
+}
+
+
+void codec_buf_struct_info_free(struct codec_buf_struct_info *info)
+{
+	for (int j = 0; j < info->n_fields; j++) {
+		codec_free(info->field_info[j].codec);
+	}
+
+	free(info);
+}
+
+void codec_buf_set(void **buf, uint i, void *item,
+                   struct codec_buf_struct_info *info)
+{
+	for (int j = 0; j < info->n_fields; j++) {
+		struct field_info f_info = info->field_info[j];
+		void *p = (char *)buf[j] + (i << f_info.logsz);
+		memcpy(p, (char *)item + f_info.offset, f_info.sz);
+	}
+}
+
+size_t codec_buf_encode(void *dest, void **src, uint n,
+                        struct codec_buf_struct_info *info)
+{
+	PTR_CAST(d, char, dest);
+
+	/* write down the length */
+	*(u16 *)d = n;
+	d += sizeof(u16);
+
+	/* encoding */
+	for (int j = 0; j < info->n_fields; j++) {
+		struct field_info f_info = info->field_info[j];
+		size_t m = (n << f_info.logsz) >> log2(sizeof(uint32_t));
+		d += codec_compress_ints(f_info.codec, (uint32_t*)src[j], m, d);
+	}
+
+	return (uintptr_t)((void*)d - dest);
+}
+
+size_t codec_buf_decode(void **dest, void *src, uint n,
+                        struct codec_buf_struct_info *info)
+{
+	PTR_CAST(s, char, src);
+
+	/* extract the length */
+	u16 l = *(u16 *)s;
+	s += sizeof(u16);
+
+	/* decoding */
+	for (int j = 0; j < info->n_fields; j++) {
+		struct field_info f_info = info->field_info[j];
+		size_t m = (l << f_info.logsz) >> 2;
+		s += codec_decompress_ints(f_info.codec, s, (uint32_t*)dest[j], m);
+	}
+
+	return (uintptr_t)((void*)s - src);
+}
---
+++ postlist/codec-buf.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "common/common.h"
+
+#define log2(_n) (31 - __builtin_clz(_n))
+
+#define FIELD_INFO(_type, _member, _codec) \
+	({ _type *p; ((struct field_info) { \
+		offsetof(_type, _member), \
+		sizeof p->_member, \
+		log2(sizeof p->_member), \
+		codec_new(_codec, CODEC_DEFAULT_ARGS) \
+	}); })
+
+struct codec;
+
+struct field_info {
+	uint offset, sz, logsz;
+	struct codec *codec;
+};
+
+struct codec_buf_struct_info {
+	uint              n_fields;
+	struct field_info field_info[];
+};
+
+void** codec_buf_alloc(uint, struct codec_buf_struct_info*);
+void codec_buf_free(void **, struct codec_buf_struct_info*);
+
+struct codec_buf_struct_info *codec_buf_struct_info_alloc(int);
+void codec_buf_struct_info_free(struct codec_buf_struct_info*);
+
+void codec_buf_set(void **, uint, void*, struct codec_buf_struct_info*);
+
+size_t codec_buf_encode(void*, void **, uint, struct codec_buf_struct_info*);
+size_t codec_buf_decode(void **, void*, uint, struct codec_buf_struct_info*);
