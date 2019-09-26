6a19267d110a46c66782df00898446df6813a2b3
t-k- <clock126@126.com>
2019-09-27 00:52:36 +0800

    remove dead code

--- postlist/math-postlist.h
+++ postlist/math-postlist.h
@@ -14,24 +14,6 @@ struct math_postlist_item {
 	/* 7 */ uint32_t  leaf_id[MAX_MATH_POSTLIST_PATHINFO_LEN];
 }; /* 1024 bytes */
 
-#define MAX_EXP_SYMBOL_SPLITS 4
-struct math_invlist_item {
-	uint32_t doc_id;
-	uint32_t sec_id; /* sector tree ID: formulaID (20 bits) appended by nodeID (12 bits) */
-	uint32_t offset; /* offset of corresponding position in another file */
-	uint16_t width; /* sector tree width appended by original formula width (n_lr_paths) */
-
-	/* --- in two separate files --- */
-	uint16_t splits; /* how many different symbol values, appended by operator signature */
-	uint16_t ophash; /* how many different symbol values, appended by operator signature */
-	/* 16 bytes */
-
-	uint16_t symbol[MAX_EXP_SYMBOL_SPLITS]; /* leaf symbol */
-	uint16_t splt_w[MAX_EXP_SYMBOL_SPLITS]; /* split width */
-	/* 32 bytes */
-	uint64_t leaves[MAX_EXP_SYMBOL_SPLITS]; /* bit mask signature (for wildcards) */
-};
-
 struct math_postlist_gener_item {
 	/* 0 */ uint32_t  exp_id; /* lower address, less significant */
 	/* 1 */ uint32_t  doc_id; /* higher address, more significant */
