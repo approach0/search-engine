e70513995081c1a092cdbe9c55428b487a0ff650
t-k- <clock126@126.com>
2019-10-14 00:58:45 +0800

    add command line arguments to indexerd.

--- indexerd/config.h
+++ indexerd/config.h
@@ -1,7 +1,8 @@
-#define INDEXD_DEFAULT_URI  "/index"
+#define INDEXD_DEFAULT_URI "/index"
+#define INDEXD_DEFAULT_DIR "./tmp"
 #define INDEXD_DEFAULT_PORT 8934
 
-#define MAINTAIN_UNFREE_CNT 5000
+#define MAINTAIN_CYCLE_CNT 500
 
 /* macro defined in "indri/src/RepositoryMaintenanceThread.cpp" */
 #define MAXIMUM_INDEX_COUNT 5
--- indexerd/run/indexerd.c
+++ indexerd/run/indexerd.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 #include "common/common.h"
 #include "mhook/mhook.h"
@@ -46,7 +47,7 @@ static int consider_index_maintain(struct indexer *indexer)
 		indexer_maintain(indexer);
 		sleep(30);
 
-	} else if ((indices->n_doc + 1) % MAINTAIN_UNFREE_CNT == 0) {
+	} else if ((indices->n_doc + 1) % MAINTAIN_CYCLE_CNT == 0) {
 		printf("\n[flushing index ...]\n");
 		fflush(stdout);
 
@@ -88,7 +89,7 @@ static const char *httpd_on_index(const char* req, void* arg_)
 	/* print maintainer related stats */
 	const int64_t unfree = mhook_unfree();
 	printf("Unfree: %ld, Doc: %u per %u, Index segments: %lu / %u, ",
-		unfree, indexer->indices->n_doc, MAINTAIN_UNFREE_CNT,
+		unfree, indexer->indices->n_doc, MAINTAIN_CYCLE_CNT,
 		term_index_size(indexer->indices->ti), MAXIMUM_INDEX_COUNT
 	);
 	printf("%lu TeX parsed, error rate: %.2f %%.", indexer->n_parse_tex,
@@ -100,27 +101,75 @@ ret:
 	return retjson;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	struct uri_handler handlers[] = {
-		{INDEXD_DEFAULT_URI, &httpd_on_index}
-	};
+	char indexd_uri[1024] = INDEXD_DEFAULT_URI;
+	struct uri_handler handlers[1];
 	struct indices indices;
 	struct indexer *indexer;
 	unsigned short port = INDEXD_DEFAULT_PORT;
+	char index_output_dir[MAX_PATH_LEN] = INDEXD_DEFAULT_DIR;
 
-	indices_open(&indices, "./tmp", INDICES_OPEN_RW);
+	int opt;
+	while ((opt = getopt(argc, argv, "ho:u:p:")) != -1) {
+		switch (opt) {
+		case 'h':
+			printf("DESCRIPTION:\n");
+			printf("index daemon. \n");
+			printf("\n");
+			printf("USAGE:\n");
+			printf("%s -h | "
+			       "-o <output> (default: " INDEXD_DEFAULT_DIR ") | "
+			       "-p <port> (default is %hu) | "
+			       "-u <index uri> (default is " INDEXD_DEFAULT_URI ")"
+			       "\n", argv[0], INDEXD_DEFAULT_PORT);
+			printf("\n");
+			printf("EXAMPLE:\n");
+			printf("%s -o ./some/where 2> err.log\n", argv[0]);
+			goto exit;
 
+		case 'o':
+			strcpy(index_output_dir, optarg);
+			break;
+
+		case 'p':
+			sscanf(optarg, "%hu", &port);
+			break;
+
+		case 'u':
+			if (optarg[0] != '/')
+				prerr("Error: URI should start with a slash.\n", 0);
+			else
+				strcpy(indexd_uri, optarg);
+			break;
+
+		default:
+			printf("bad argument(s). \n");
+			goto exit;
+		}
+	}
+
+	/* open specified indices */
+	printf("open indices at %s ...\n", index_output_dir);
+	indices_open(&indices, index_output_dir, INDICES_OPEN_RW);
+
+	/* create indexer for specified indices */
 	indexer = indexer_alloc(&indices, &lex_eng_file, &parser_exception);
+
+	/* prepare and spawn httpd */
+	strcpy(handlers[0].uri, indexd_uri);
+	handlers[0].handler = &httpd_on_index;
 
 	printf("listening at %u:%s ...\n", port, handlers[0].uri);
 	httpd_run(port, handlers,
 		sizeof handlers / sizeof(struct uri_handler), indexer);
 
+	/* free resources */
 	printf("closing index...\n");
 	indexer_free(indexer);
 	indices_close(&indices);
 
+exit:
 	mhook_print_unfree();
 	return 0;
 }
