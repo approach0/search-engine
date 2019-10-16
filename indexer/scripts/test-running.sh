9966dfb43e987d4b83c9e4498e5bac5aec63ae68
t-k- <clock126@126.com>
2019-10-17 02:40:36 +0800

    test-running.sh takes PID.

--- indexer/scripts/test-running.sh
+++ indexer/scripts/test-running.sh
@@ -4,15 +4,15 @@ if [ "$1" == "-h" ]; then
 cat << USAGE
 Description:
 Test the elapsed time for a process since running.
-$0 <process name>
+$0 <PID>
 
 Examples:
-$0 indexerd.out
+$0 \$(ps a -o comm,pid | grep "json-feeder.py" | awk '{print \$2}')
 USAGE
 exit
 fi
 
 [ $# -ne 1 ] && echo 'bad arg.' && exit
 
-PROCESS="$1"
-while pidof "$PROCESS" >/dev/null; do ps --pid $(pidof "$PROCESS") -o command,etime; date; sleep 1; done
+PID=$1
+while ps --pid $PID >/dev/null; do ps --pid $PID -o comm,etime; date; sleep 1; done
