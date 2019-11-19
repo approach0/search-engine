ef99a135cbd083586694f0b97b6a0357b25fee88
t-k- <clock126@126.com>
2019-11-19 23:04:47 +0800

    implemented podium merger.

--- merger/podium-merger.c
+++ merger/podium-merger.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <float.h>
-#include "common/common.h"
+#include <float.h> /* for FLT_MAX */
+#include "common/common.h" /* for MAX() */
 #include "mergers.h"
 
 int pd_merger_lift_up_pivot(struct pd_merger *m, float threshold,
@@ -9,7 +9,8 @@ int pd_merger_lift_up_pivot(struct pd_merger *m, float threshold,
 {
 	for (int i = m->pivot; i >= 0; i--) {
 		float sum = m->acc_upp[i];
-		if (relax(arg, sum) > threshold) {
+		float max = m->acc_max[i];
+		if (relax(arg, max + sum) > threshold) {
 			m->pivot = i;
 			break;
 		}
@@ -18,13 +19,16 @@ int pd_merger_lift_up_pivot(struct pd_merger *m, float threshold,
 	return m->pivot;
 }
 
-void pd_merger_update_acc_upp(struct pd_merger *m)
+static void pd_merger_update_acc_values(struct pd_merger *m)
 {
 	float sum = 0, max = -FLT_MAX;
 	for (int i = m->size - 1; i >= 0; i--) {
 		float upp = merger_map_prop(m, upp, i);
+		float pd_upp = merger_map_prop(m, pd_upp, i);
 		sum += upp;
+		max = MAX(max, pd_upp);
 		m->acc_upp[i] = sum;
+		m->acc_max[i] = max;
 	}
 }
 
@@ -38,7 +42,7 @@ int pd_merger_map_remove(struct pd_merger *m, int i)
 	for (int j = i; j < m->size; j++)
 		m->map[j] = m->map[j + 1];
 
-	pd_merger_update_acc_upp(m);
+	pd_merger_update_acc_values(m);
 	return i - 1;
 }
 
@@ -69,8 +73,6 @@ static void pd_merger_iter_sort(struct pd_merger *m)
 			}
 		}
 	}
-
-	pd_merger_update_acc_upp(m);
 }
 
 uint64_t pd_merger_min(struct pd_merger *m)
@@ -95,7 +97,8 @@ struct pd_merger *pd_merger_iterator(merge_set_t* set)
 	m->pivot = set->n - 1;
 	m->min = pd_merger_min(m);
 
-	pd_merger_iter_sort(m); /* acc_upp is also updated */
+	pd_merger_iter_sort(m);
+	pd_merger_update_acc_values(m);
 	return m;
 }
 
@@ -124,15 +127,22 @@ int pd_merger_iter_next(struct pd_merger *m)
 
 void pd_merger_iter_print(struct pd_merger* m, merger_keyprint_fun keyprint)
 {
+	/* header first */
+	printf("%c | %5s | %-6s %5s | %-6s %5s [%3s]\n", 'F',
+		"score", "accmax", "pdupp", "accupp", "upp", "inv");
+
 	for (int i = 0; i < m->size; i++) {
 		int invi = m->map[i];
 		int pivot = m->pivot;
 		float acc_upp = m->acc_upp[i];
+		float acc_max = m->acc_max[i];
 		float upp = merger_map_prop(m, upp, i);
+		float pd_upp = merger_map_prop(m, pd_upp, i);
 		uint64_t cur = merger_map_call(m, cur, i);
 		char flag = ' ';
 		if (i == pivot) flag = 'P'; else if (i > pivot) flag = 'S';
-		printf("%c %-6.2f %5.2f [%3d] ", flag, acc_upp, upp, invi);
+		printf("%c | %5.2f | %-6.2f %5.2f | %-6.2f %5.2f [%3d] ",
+			flag, pd_upp + upp, acc_max, pd_upp, acc_upp, upp, invi);
 		if (NULL != keyprint)
 			keyprint(cur);
 		else
