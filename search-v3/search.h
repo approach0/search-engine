7d614d9c28e4fdc306c57c1133fc6a0d5346c80d
t-k- <clock126@126.com>
2019-12-10 01:42:07 +0800

    Synchronize index stats when runing cluster search.
    Without doing this, each searchd instance may have different
    document/path frequency resulting a asynchronized scores.
    
    Example bad query: x^2
    Expected top result contains x^2
    Actual top result is L^2

--- search-v3/search.h
+++ search-v3/search.h
@@ -1,16 +1,22 @@
 #pragma once
+
 /* dependency headers */
 #include "indices-v3/config.h" /* for INDICES_TXT_LEXER */
 #include "indices-v3/indices.h"
+#include "merger/config.h" /* for MAX_MERGE_SET_SZ */
 #include "query.h"
 #include "rank.h"
 
+/* stats sync array for parallel search */
 typedef struct {
 	uint32_t docN;
 	uint32_t avgDocLen;
 	uint32_t pathN;
-	uint    *doc_freq;
+	uint32_t doc_freq[MAX_MERGE_SET_SZ];
 } indices_run_sync_t;
 
+#define MAX_SYNC_ARR_LEN (sizeof(indices_run_sync_t) / sizeof(uint32_t))
+
+/* indices query function */
 ranked_results_t
 indices_run_query(struct indices*, struct query*, indices_run_sync_t *, int);
