281cd81cdfdf6a0d552ebb954c44cecab0d8b703
t-k- <clock126@126.com>
2020-09-19 02:03:30 +0800

    Docker-build.sh: minor improvement

--- Docker-build.sh
+++ Docker-build.sh
@@ -10,6 +10,6 @@ docker images
 docker build --network host --tag ${IMAGE_NAME} .
 
 echo "==== Use commands below to run or modify ===="
-echo docker run --network host -it ${IMAGE_NAME} /usr/bin/indexerd.out
+echo docker run --network host -v '`pwd`'/tmp:/mnt/index -it ${IMAGE_NAME} indexerd.out -o /mnt/index
 echo docker run --network host -it ${IMAGE_NAME} /bin/bash
 echo docker commit $(docker ps -q --latest) ${IMAGE_NAME}
