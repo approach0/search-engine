2515b1516979e6d880de2d2098492538b8c13417
t-k- <clock126@126.com>
2021-03-04 12:52:34 +0800

    pya0: added pya0/dep/dep-z.mk

--- pya0/tests/test-lexer.py
+++ pya0/tests/test-lexer.py
@@ -3,4 +3,5 @@ sys.path.insert(0, './lib')
 
 import pya0
 tokens = pya0.lex("a + \\frac b {a + 1} \n a^2 + b^2 = {c}^2")
-print(tokens)
+for tokenID, tokenType, symbol in tokens:
+	print(tokenID, tokenType, symbol)
