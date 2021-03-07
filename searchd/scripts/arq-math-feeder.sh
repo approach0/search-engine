b784bc4a38d9e0f7e11debb10db9b528dcc38bf1
t-k- <clock126@126.com>
2021-03-07 07:51:52 +0000

    arq-math-feeder.sh: skip topic if file not exists.

--- searchd/scripts/arq-math-feeder.sh
+++ searchd/scripts/arq-math-feeder.sh
@@ -3,6 +3,9 @@ OUTFILE=arq2020-task1-a0-submission.tsv
 > $OUTFILE
 for i in {1..100}; do
 	echo "Query ${i}"
-	./test-query.sh "http://localhost:8921/search" ../arq-math-task1-queries/${i}.json
-	cat ../trec-format-results.tmp | awk -v OFS="\t" '{print $1, $3, $4, $5, $6}' | sed "s/_QRY_ID_/A.${i}/g" >> $OUTFILE
+	target=../arq-math-task1-queries/${i}.json
+	if [ -e $target ]; then
+		./test-query.sh "http://localhost:8921/search" $target
+	fi
+	#cat ../trec-format-results.tmp | awk -v OFS="\t" '{print $1, $3, $4, $5, $6}' | sed "s/_QRY_ID_/A.${i}/g" >> $OUTFILE
 done
